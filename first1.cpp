62688d83b7f98b041b38235397fa2ad86e1abe02
Harshita51997 <35426310+Harshita51997@users.noreply.github.com>
2018-01-18 23:30:29 +0530

    Add files via upload

---
+++ first1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+ printf("Enter the no");
+ scanf("%d",&n);
+ 
+ if(n>0)
+ {
+ printf("the no is positvie");
+}
+ if(n==0)
+ {
+   printf("the no is zero");
+}
+if(n<0)
+{
+       printf("the no is negative");
+       }
+       return 0;
+       }
